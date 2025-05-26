903e3361d71fcf4e582644466d571ed8ae35e1ec
HumanAI <drapeta@gmail.com>
2025-05-26 12:29:32 -0400

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,1 +1,7 @@
-
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('start-button').addEventListener('click', () => {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+      chrome.tabs.sendMessage(tabs[0].id, { action: 'startOverlay' });
+    });
+  });
+});
